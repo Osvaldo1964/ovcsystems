d9e9441ff0c790d8049d3ab9f357b3c25c44d739
Osvaldo1964 <140724795+Osvaldo1964@users.noreply.github.com>
2026-01-02 08:25:49 -0500

    initial_ok

---
+++ ovcauth/Config/Config.php
@@ -0,0 +1,12 @@
+<?php
+const BASE_URL = "http://ovcauth.ovcsystems.com/";
+
+//Datos de conexión a Base de Datos
+const CONNECTION = true;
+const DB_HOST = "localhost";
+const DB_NAME = "db-autentication";
+const DB_USER = "root";
+const DB_PASSWORD = "";
+const DB_CHARSET = "utf8";
+
+const KEYJWT = "AutenticacionJWT2024"; //Clave para codificar y decodificar el token JWT
